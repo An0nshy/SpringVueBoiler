cff1745a16397d9063379316070ae6b0ec8889f3
an0nshy <anonshy@yandex.com>
2021-03-22 18:38:53 +0100

    clean up config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,26 +3,12 @@ const webpack = require("webpack")
 
 module.exports = {
   outputDir: path.resolve(__dirname, "../src/main/resources/public"),
-  //assetsDir: path.resolve(__dirname, "../src/main/resources/public/img"),
-  //indexPath: "../templates/vue/index.html",// keep in root/src/
   pages: {
     user: {
       entry: 'Modules/user/main.js',
       template: 'src/public/index.html',
       filename: '../templates/vue/user.html',
     },
-  },
-  chainWebpack: config => {
-    config.plugin('copy')
-      .tap(args => {
-        args[0].push({
-          from: path.resolve(__dirname, 'public/'),
-          to: path.resolve(__dirname, '../src/main/resources/public/img'),
-          toType: 'dir',
-          ignore: ['.DS_Store']
-        })
-        return args
-      })
   },
   css: {
     extract: {
